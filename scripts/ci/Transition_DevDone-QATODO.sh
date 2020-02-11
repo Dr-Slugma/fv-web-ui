b089d10550da61c5981d1acd05cbcfce5dc320a8
Nolan Van Hell <nolan@fpcc.ca>
2020-02-11 15:31:34 -0800

    Add script

---
+++ scripts/ci/Transition_DevDone-QATODO.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# This script will perform the actions needed to transition the appropriate issues from
+# DEV DONE to QA TO DO
+
+echo "value: ${{ steps.commit-message.outputs.value }}"
